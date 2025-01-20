a5dc86cedd0001407d0585890bb8c96df31a585c
Keerthi <110728322+Keerthigavijayakrishnan@users.noreply.github.com>
2025-01-20 17:02:20 +0530

    Add files via upload
    Week1-Day1 Home Assignment
    DataType and Variables in Java

---
+++ Chrome.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+// HomeAssignment1
+
+public class Chrome {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Chrome");
+		
+		float version = 91.0f;
+		System.out.println("Version: " +version);
+		
+		String developer = "Google";
+		System.out.println("Developer: " +developer);
+		
+		boolean isBeta = false;
+		System.out.println("Is Beta: " +isBeta);
+		
+		int releaseYear = 2008;
+		System.out.println("Release Year: " +releaseYear);
+		
+		char shortcutKey = 'c';
+		System.out.println("Shortcut Key: " +shortcutKey);
+		
+	}
+
+}
