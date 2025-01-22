26cf1bbe1ea2ef2bcfc5c7a818fb6a17d99aece6
Keerthi <110728322+Keerthigavijayakrishnan@users.noreply.github.com>
2025-01-23 01:17:01 +0530

    PrintSameValues.java

--- day3/PrintSameValues.java
+++ day3/PrintSameValues.java
@@ -6,17 +6,17 @@ import java.util.Arrays;
 public class PrintSameValues {
 	public static void main(String[] args) {
 		
-		int a[] = {3,2,11,4,6,7};                            //012345
-		Arrays.sort(a);                                      //2,3,4,6,7,11
-		int b[] = {1,2,8,4,9,7};                             //1,2,4,7,8,9
+		int a[] = {3,2,11,4,6,7};                            
+		Arrays.sort(a);                                     
+		int b[] = {1,2,8,4,9,7};                             
 		Arrays.sort(b);
 		System.out.println("Mathing elements in two arrays:");
 		
 		for(int i=0; i<a.length; i++) {                      
 			
-			for(int j=0; j<b.length; j++) {                  //once the for loop ends then only the next loop continue. i1 start and j1..6 continue and end->i2 start and j1..6 continue and end->i3..like this.
+			for(int j=0; j<b.length; j++) {                  
 				
-				if (a[i]==b[j]) {                            //a[0]2=b[0]1,a[0]2=b[1]2,a[0]2=b[2]4,a[0]2=b[3]7,a[0]2=b[3]8,a[0]2=b[3]9. once completed the flow,it goes to for(i loop) then a[1]2=b[0]1,a[1]2=b[1]2,a[1]2=b[2]4,a[1]2=b[3]7,a{1]2=b[3]8,a[1]2=b[3]9..it will continue
+				if (a[i]==b[j]) {                           
 					System.out.println(a[i]);
 				}
 			}
