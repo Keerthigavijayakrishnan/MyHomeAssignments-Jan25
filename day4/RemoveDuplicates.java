0f7fc8c1d5fd7cdc66aae1badd2dd854b0265dd7
Keerthi <110728322+Keerthigavijayakrishnan@users.noreply.github.com>
2025-01-24 18:44:01 +0530

    RemoveDuplicates.java

--- day4/RemoveDuplicates.java
+++ day4/RemoveDuplicates.java
@@ -6,11 +6,11 @@ public class RemoveDuplicates {
 	public static void main(String[] args) {
 		
 		String text = "We learn Java basics as part of java sessions in java week1"; 
-		             // 0   1    2     3    4   5   6    7     8      9  10   11 
+		             
 		  System.out.println("String Input: "+text);
 		  System.out.print("String Output: ");   
 		  
-		   String[] split = text.split(" ");                     //length=12
+		   String[] split = text.split(" ");                     
   
 		      
 		   //	System.out.println(split[3]);
@@ -19,11 +19,11 @@ public class RemoveDuplicates {
 	        for (int j=i+1; j<split.length; j++)  { 
 	        //	 System.out.println(split[j]);
 	           if (split[i].equalsIgnoreCase(split[j])) {  
-	             split[j]="";                                       //Assign to the text directly
+	             split[j]="";                                       
 				  
-	           }        	   	        	                           //Inside for loop, we changed split[index] value.
+	           }        	   	        	                         
 	           }
-	           } String result = String.join(" ", split);               //Convert from string[] into string using join
+	           } String result = String.join(" ", split);               
 	           System.out.println(result);
 		 }
 
